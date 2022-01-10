d6f14345edbcc852bb6302f5156dd1e8330489ce
Yash Patidar <yashpatidar040@gmail.com>
2022-01-11 01:36:49 +0530

    implemented security framework with sql with 2 endpoint

--- src/main/java/com/consultadd/config/WebSecurityConfig.java
+++ src/main/java/com/consultadd/config/WebSecurityConfig.java
@@ -31,9 +31,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/")
-                .permitAll()
                 .antMatchers("/employee")
+                .permitAll()
+                .antMatchers("/employee/{age}")
                 .hasAuthority("USER")
                 .anyRequest()
                 .authenticated()
