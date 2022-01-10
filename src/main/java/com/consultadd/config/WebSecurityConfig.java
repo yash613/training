60e083c639145b1c95b4f4a5bc4d912d527a385d
Yash Patidar <yashpatidar040@gmail.com>
2022-01-11 01:33:26 +0530

    implemented security framework with sql

--- src/main/java/com/consultadd/config/WebSecurityConfig.java
+++ src/main/java/com/consultadd/config/WebSecurityConfig.java
@@ -1,27 +1,43 @@
 package com.consultadd.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
-
+@Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance())
-                .withUser("user1").password("password").roles("USER")
-                .and().withUser("user2").password("yash").roles("ADMIN");
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider=new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(new BCryptPasswordEncoder());
+        return provider;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-       http
-           .authorizeRequests()
-           .antMatchers("/employee").hasRole("USER")
-           .and().formLogin();
+        http.authorizeRequests()
+                .antMatchers("/")
+                .permitAll()
+                .antMatchers("/employee")
+                .hasAuthority("USER")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
     }
 }
